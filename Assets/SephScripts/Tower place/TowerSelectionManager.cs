81ad8228a56e1290aee01dd667ffb8b56201bb15
JosephEmer05 <ej.tarlengco@gmail.com>
2025-11-21 17:13:25 +0800

    Tower Placement

---
+++ Assets/SephScripts/Tower place/TowerSelectionManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TowerSelectionManager : MonoBehaviour
+{
+    public static TowerSelectionManager Instance;
+
+    private GameObject selectedTower;
+    private List<TowerSlot> allSlots = new List<TowerSlot>();
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void RegisterSlot(TowerSlot slot)
+    {
+        allSlots.Add(slot);
+        slot.HideSlot();
+    }
+
+    public void SelectTower(GameObject tower)
+    {
+        if (selectedTower == tower)
+        {
+            DeselectTower();
+            return;
+        }
+
+        selectedTower = tower;
+        ShowSlots();
+    }
+
+    public void DeselectTower()
+    {
+        selectedTower = null;
+        HideSlots();
+    }
+
+    public void TryPlaceTower(TowerSlot slot)
+    {
+        if (selectedTower == null) return;
+
+        if (!slot.isOccupied)
+        {
+            selectedTower.transform.position = slot.transform.position;
+            slot.isOccupied = true;
+            DeselectTower();
+        }
+    }
+
+    private void ShowSlots()
+    {
+        foreach (var slot in allSlots)
+            slot.ShowSlot();
+    }
+
+    private void HideSlots()
+    {
+        foreach (var slot in allSlots)
+            slot.HideSlot();
+    }
+}
--- Assets/SephScripts/Tower place/TowerSlot.cs
+++ Assets/SephScripts/Tower place/TowerSlot.cs
@@ -4,16 +4,48 @@ public class TowerSlot : MonoBehaviour
 {
     public bool isOccupied = false;
 
-    public bool PlaceTower(GameObject tower)
-    {
-        if (isOccupied)
-        {
-            Debug.Log("Slot already occupied.");
-            return false;
-        }
+    private MeshRenderer mr;
+    private Color defaultColor;
 
-        tower.transform.position = transform.position;
-        isOccupied = true;
-        return true;
+    public Color availableColor = Color.blue;
+    public Color occupiedColor = Color.red;
+
+    void Start()
+    {
+        mr = GetComponent<MeshRenderer>();
+        defaultColor = mr.material.color;
+
+        TowerSelectionManager.Instance.RegisterSlot(this);
+    }
+
+    public void ShowSlot()
+    {
+        mr.enabled = true;
+    }
+
+    public void HideSlot()
+    {
+        mr.enabled = false;
+    }
+
+    private void OnMouseEnter()
+    {
+        if (!mr.enabled) return;
+
+        mr.material.color = isOccupied ? occupiedColor : availableColor;
+    }
+
+    private void OnMouseExit()
+    {
+        if (!mr.enabled) return;
+
+        mr.material.color = defaultColor;
+    }
+
+    private void OnMouseDown()
+    {
+        if (!mr.enabled) return;
+
+        TowerSelectionManager.Instance.TryPlaceTower(this);
     }
 }
